b85c3b6a8b0eb3b569a05ab128c23925cd00716c
Aliisa <aliisarokala@protonmail.com>
2024-09-03 10:42:25 +0300

    Task 3 update

---
+++ Tasks/Task3CompositeMethod/Component.java
@@ -0,0 +1,20 @@
+package Task3CompositeMethod;
+
+//Abstract class for both Departments and Employees.
+public abstract class Component {
+    protected String name;
+
+    public Component(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public abstract void add(Component component);
+    public abstract void remove(Component component);
+    public abstract int getTotalSalary();
+    public abstract void printStructure(String indent);
+    public abstract String toXML();
+}
