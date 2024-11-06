7840e94e16f5bca0dd62e983a9411be513452630
Aliisa <aliisarokala@protonmail.com>
2024-11-06 09:10:12 +0200

    Task 13 update

--- src/Task13Visitor/Main.java
+++ src/Task13Visitor/Main.java
@@ -3,19 +3,24 @@ package Task13Visitor;
 public class Main {
     public static void main(String[] args) {
         // Create directories and files
-        Directory root = new Directory();
-        Directory sub = new Directory();
+        Directory root = new Directory();  // root directory
+        Directory sub1 = new Directory();  // First subdirectory
+        Directory sub2 = new Directory();  // Second subdirectory
+
         File fileA = new File("fileA.txt", 10);
         File fileB = new File("fileB.txt", 20);
-        File fileC = new File("fileC.jpg", 30);
-        File fileD = new File("fileD.png", 40);
+        File fileC = new File("fileC.txt", 30);
+        File fileD = new File("fileD.jpg", 40);
+        File fileE = new File("fileE.png", 50);
 
         // Build the file system structure
-        root.addElement(fileA);
-        root.addElement(sub);
-        sub.addElement(fileB);
-        sub.addElement(fileC);
-        sub.addElement(fileD);
+        root.addElement(sub1);
+        root.addElement(sub2);
+        sub1.addElement(fileA);
+        sub1.addElement(fileB);
+        sub1.addElement(fileC);
+        sub2.addElement(fileD);
+        sub2.addElement(fileE);
 
         // Create a SizeCalculatorVisitor
         SizeCalculatorVisitor sizeCalculatorVisitor = new SizeCalculatorVisitor();
@@ -24,8 +29,8 @@ public class Main {
         // Output the total size of files
         System.out.println("Total size of all files: " + sizeCalculatorVisitor.getTotalSize() + " MB");
 
-        // Create and apply SearchVisitor that searches for fileD.png as well as files that end with.jpg
-        SearchVisitor searchVisitor = new SearchVisitor("fileD.png", ".jpg");
+        // Create and apply SearchVisitor that searches for fileE.png as well as files that end with .jpg
+        SearchVisitor searchVisitor = new SearchVisitor("fileE.png", ".jpg");
         root.accept(searchVisitor);
 
         // Output the search results
@@ -36,5 +41,3 @@ public class Main {
         }
     }
 }
-
-
